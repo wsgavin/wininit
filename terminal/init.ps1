a2798ca21c7cef13fa6476003cdf2d500fb2d48c
Warren <warren@dubelyoo.com>
2025-08-12 16:18:00 -0700

    Updating...

--- terminal/init.ps1
+++ terminal/init.ps1
@@ -1,6 +1,6 @@
 $schemes = Get-Content -Path ".\schemes.json" -Raw | ConvertFrom-Json
 $themes = Get-Content -Path ".\themes.json" -Raw | ConvertFrom-Json
-$profiles_defaults = Get-Content -Path ".\profile.defaults.json" -Raw | ConvertFrom-Json
+$profiles_defaults = Get-Content -Path ".\profiles.defaults.json" -Raw | ConvertFrom-Json
 
 
 $terminal_settings = Get-Content -Path "$ENV:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Raw | ConvertFrom-Json
@@ -9,3 +9,7 @@ $terminal_settings = Get-Content -Path "$ENV:LOCALAPPDATA\Packages\Microsoft.Win
 $terminal_settings.schemes = $schemes
 $terminal_settings.themes = $themes
 $terminal_settings.profiles.defaults = $profiles_defaults
+
+$json = $terminal_settings | ConvertTo-Json -Depth 10
+
+$json | Set-Content -Path "$ENV:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
