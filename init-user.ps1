e0f9e568bf72f1882ee5c2cdce8ddc845faf2475
Warren <warren@dubelyoo.com>
2025-09-23 10:12:16 -0500

    Updated

--- init-user.ps1
+++ init-user.ps1
@@ -1,58 +1,17 @@
-$nvimConfigDirectory = "$env:XDG_CONFIG_HOME\nvim"
 
-if (-not (Test-Path -Path $nvimConfigDirectory -PathType Container)) {
+# Application Setups
 
-  Write-Host "Installing LazyVim"
-  git clone https://github.com/LazyVim/starter $nvimConfigDirectory
-  Remove-Item $nvimConfigDirectory\.git -Recurse -Force
+.\win11\user\applications\nvim.ps1
+.\win11\user\applications\oh-my-posh.ps1
+.\win11\user\applications\ssh.ps1
+.\win11\user\applications\git.ps1
+.\win11\user\applications\code.ps1
+.\win11\user\applications\terminal.ps1
 
-}
-else {
-  Write-Host "SKIPPING: LazyVim already exists."
-}
+# User Preferences
 
-oh-my-posh font install CascadiaMono
-
-ssh-add $env:USERPROFILE\.ssh\id_ed25519
-
-# git Configuration
-
-git config --global user.email "warren@dubelyoo.com"
-git config --global user.name "Warren"
-git config --global core.autocrlf false
-git config --global core.eol lf
-
-# vscode Extensions
-
-code  --install-extension bbenoist.Nix
-code  --install-extension brettm12345.nixfmt-vscode
-code  --install-extension DavidAnson.vscode-markdownlint
-code  --install-extension ms-vscode.powershell
-code  --install-extension esbenp.prettier-vscode
-code  --install-extension dbaeumer.vscode-eslint
-code  --install-extension ms-python.python
-code  --install-extension EditorConfig.EditorConfig
-
-# Microsoft Terminal Configurations
-
-$terminalDirectory = ".\terminal"
-$terminalSettingsFile = `
-  "$ENV:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-$actions = Get-Content -Path "$terminalDirectory\actions.json" -Raw | ConvertFrom-Json
-$profiles_defaults = Get-Content -Path "$terminalDirectory\profiles.defaults.json" -Raw | ConvertFrom-Json
-$schemes = Get-Content -Path "$terminalDirectory\schemes.json" -Raw | ConvertFrom-Json
-$themes = Get-Content -Path "$terminalDirectory\themes.json" -Raw | ConvertFrom-Json
-
-$terminal_settings = Get-Content -Path "$terminalSettingsFile" -Raw | ConvertFrom-Json
-
-$terminal_settings.actions = $actions
-$terminal_settings.profiles.defaults = $profiles_defaults
-$terminal_settings.schemes = $schemes
-$terminal_settings.themes = $themes
-
-$json = $terminal_settings | ConvertTo-Json -Depth 10
-$json | Set-Content -Path "$terminalSettingsFile"
+.\win11\user\settings\desktop.ps1
+.\win11\user\settings\taksbar.ps1
 
 
 # Pull down repositories
