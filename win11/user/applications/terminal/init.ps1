e30b4413c16c9aecdbe7a2f886869bcfdb34370b
Warren <warren@dubelyoo.com>
2025-09-26 14:10:43 -0500

    Updating

--- win11/user/applications/terminal/init.ps1
+++ win11/user/applications/terminal/init.ps1
@@ -1,5 +1,33 @@
 # Microsoft Terminal Configurations
 
+#
+# Add-KeyValuePair Updates or adds a key and the associate value to a psobject.
+#
+function Add-KeyValuePair {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [psobject]$json,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Key,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Value
+  )
+
+  if ($json.psobject.Properties[$Key] -and !$Force) {
+    $json.$Key = $Value
+  }
+  else {
+    $json | Add-Member -MemberType NoteProperty -Name $Key -Value $Value
+  }
+
+}
+
+
+
+
 $terminalSettingsFile = `
   "$ENV:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
@@ -11,61 +39,61 @@ $themes = Get-Content -Path "$PSScriptRoot\json\themes.json" -Raw | ConvertFrom-
 $terminal_settings = Get-Content -Path "$terminalSettingsFile" -Raw | ConvertFrom-Json
 
 
-if ($null -ne $terminal_settings.'compatibility.enableUnfocusedAcrylic') {
-  $terminal_settings.'compatibility.enableUnfocusedAcrylic' = $true
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "compatibility.enableUnfocusedAcrylic" -Value $true
-}
+# if ($null -ne $terminal_settings.'compatibility.enableUnfocusedAcrylic') {
+#   $terminal_settings.'compatibility.enableUnfocusedAcrylic' = $true
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "compatibility.enableUnfocusedAcrylic" -Value $true
+# }
 
-if ($null -ne $terminal_settings.copyFormatting) {
-  $terminal_settings.copyFormatting = "none"
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "copyFormatting" -Value "none"
-}
+# if ($null -ne $terminal_settings.copyFormatting) {
+#   $terminal_settings.copyFormatting = "none"
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "copyFormatting" -Value "none"
+# }
 
-if ($null -ne $terminal_settings.copyOnSelect) {
-  $terminal_settings.copyOnSelect = $true
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "copyOnSelect" -Value $true
-}
+# if ($null -ne $terminal_settings.copyOnSelect) {
+#   $terminal_settings.copyOnSelect = $true
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "copyOnSelect" -Value $true
+# }
 
-if ($null -ne $terminal_settings.'experimental.enableColorSelection') {
-  $terminal_settings.'experimental.enableColorSelection' = $true
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "experimental.enableColorSelection" -Value $true
-}
+# if ($null -ne $terminal_settings.'experimental.enableColorSelection') {
+#   $terminal_settings.'experimental.enableColorSelection' = $true
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "experimental.enableColorSelection" -Value $true
+# }
 
-if ($null -ne $terminal_settings.focusFollowMouse) {
-  $terminal_settings.focusFollowMouse = $true
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "focusFollowMouse" -Value $true
-}
+# if ($null -ne $terminal_settings.focusFollowMouse) {
+#   $terminal_settings.focusFollowMouse = $true
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "focusFollowMouse" -Value $true
+# }
 
-if ($null -ne $terminal_settings.searchWebDefaultQueryUrl) {
-  $terminal_settings.searchWebDefaultQueryUrl = "https://www.google.com/search?q=%22%s%22"
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "searchWebDefaultQueryUrl" -Value "https://www.google.com/search?q=%22%s%22"
-}
+# if ($null -ne $terminal_settings.searchWebDefaultQueryUrl) {
+#   $terminal_settings.searchWebDefaultQueryUrl = "https://www.google.com/search?q=%22%s%22"
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "searchWebDefaultQueryUrl" -Value "https://www.google.com/search?q=%22%s%22"
+# }
 
-if ($null -ne $terminal_settings.trimBlockSelection) {
-  $terminal_settings.trimBlockSelection = $true
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "trimBlockSelection" -Value $true
-}
+# if ($null -ne $terminal_settings.trimBlockSelection) {
+#   $terminal_settings.trimBlockSelection = $true
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "trimBlockSelection" -Value $true
+# }
 
-if ($null -ne $terminal_settings.trimPaste) {
-  $terminal_settings.trimPaste = $true
-}
-else {
-  $terminal_settings | Add-Member -MemberType NoteProperty -Name "trimPaste" -Value $true
-}
+# if ($null -ne $terminal_settings.trimPaste) {
+#   $terminal_settings.trimPaste = $true
+# }
+# else {
+#   $terminal_settings | Add-Member -MemberType NoteProperty -Name "trimPaste" -Value $true
+# }
 
 # $terminal_settings.copyFormatting = "none"
 # $terminal_settings.copyOnSelect = $true
@@ -74,6 +102,14 @@ else {
 # $terminal_settings.searchWebDefaultQueryUrl = "https://www.google.com/search?q=%22%s%22"
 # $terminal_settings.trimBlockSelection = $true
 # $terminal_settings.trimPaste = $true
+
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "copyFormatting" -Value "none"
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "copyOnSelect" -Value $true
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "experimental.enableColorSelection" -Value $true
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "focusFollowMouse" -Value $true
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "searchWebDefaultQueryUrl" -Value "https://www.google.com/search?q=%22%s%22"
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "trimBlockSelection" -Value $true
+$terminal_settings = $terminal_settings | Add-KeyValuePair -Key "trimPaste" -Value $true
 
 $terminal_settings.actions = $actions
 $terminal_settings.profiles.defaults = $profiles_defaults
